87182b3f50d80d7035f6ed1cf37cf1db28f430eb
Akash Parmar <akashparmar@FVFGV634Q05N.local>
2023-05-25 16:03:59 +0530

    aes gcm python utility

--- utilities/aes-gcm-python-utility/app.py
+++ utilities/aes-gcm-python-utility/app.py
@@ -7,7 +7,7 @@ if __name__=="__main__":
     println=builtins.print
     keypair1=key_util.generate_key_pair()
     keypair2=key_util.generate_key_pair()
-      
+    
     shared_key1=key_util.generate_shared_key(keypair1.private_key,keypair2.public_key)
     shared_key2=key_util.generate_shared_key(keypair2.private_key,keypair1.public_key)
     println("shared_key1:",shared_key1)
@@ -18,12 +18,10 @@ if __name__=="__main__":
     raw_data = "Hello This is ONDC Test Data"
     
     println("-----------------------------------------------")
-    # encrypted_data=encryption_util.encrypt_data(shared_key1,raw_data)
-    # println("Encrypted Data ===> ", encrypted_data)
+    encrypted_data=encryption_util.encrypt_data(shared_key1,raw_data)
+    println("Encrypted Data ===> ", encrypted_data)
     println("-----------------------------------------------")
     
-    encrypted_data="eyJlbmNyeXB0ZWRfZGF0YSI6IjJ0RW1mNkJWOUl2NjNqaExickV4M1JyNyIsImhtYWMiOiJDZkxxVHJKZDZkUll5cTVjV2VIVldBPT0iLCJub25jZSI6Im0wSEZQN0RTSWIySzNrU1kifQ=="
-    shared_key2="i6HWpKPkvGfQEkCDkHzzY1Ozf8e7qemY8LCjk16k9Tg="
     decrypted_data=encryption_util.decrypt_data(shared_key2,encrypted_data)
     println("decrypted Data ===> ",decrypted_data)
     println("-----------------------------------------------")
